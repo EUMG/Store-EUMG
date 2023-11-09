21d80d8db631bc286a6960d15484297b4c383c67
EUMG <edgar.molina@coppel.com>
2023-11-09 14:45:12 -0700

    Use routers instead of router

---
+++ backend/src/routers/food.router.ts
@@ -0,0 +1,41 @@
+import {Router} from 'express';
+import { sample_foods, sample_tags } from '../data';
+
+const router = Router();
+
+router.get("/",(req,res)=>{
+    res.send(sample_foods);
+})
+
+router.get("/search/:searchTerm",(req,res)=>{
+    const searchTerm = req.params.searchTerm;
+    const foods = sample_foods
+    .filter(food => food.name.toLowerCase()
+    .includes(searchTerm.toLowerCase()));
+    res.send(foods);
+})
+
+router.get("/tags",(req,res)=>{
+    res.send(sample_tags);
+})
+
+router.get("/tag/:tagName", (req, res) => {
+    const tagName = req.params.tagName;
+    const foods = sample_foods;
+
+    if (tagName === "All") {
+      res.send(foods);
+    } else {
+      const filteredFoods = foods.filter((food) => food.tags?.includes(tagName));
+      res.send(filteredFoods);
+    }
+})
+
+router.get("/:foodId",(req,res) =>{
+    const foodId = req.params.foodId;
+    const food = sample_foods
+    .find(food => food.id == foodId);
+    res.send(food);
+})
+
+export default router;
